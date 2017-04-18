5b294562ca9f49d0fb3cf519208f904d707242d8
alwayslately <theknotyouknow@163.com>
2017-04-18 23:02:33 +0800

    once again to solve the Richie Rich

--- richie_rich.py
+++ richie_rich.py
@@ -7,6 +7,60 @@ n, k = raw_input().strip().split(' ')
 n, k = [int(n), int(k)]
 number = raw_input().strip()
 
+
+number = map(lambda x: int(x), list(number))
+flag = 0
+
+for i in xrange(n / 2):
+    if k < 0:
+        print -1
+        flag = 1
+    else:
+        if number[i] == number[n - i - 1] and number[i] != 9:
+            k -= 2
+            if k < 0:
+                k += 2
+            else:
+                number[i] = 9
+                number[n - i - 1] = 9
+        elif number[i] == number[n - i - 1] and number[i] == 9:
+            pass
+        elif number[i] != number[n - i - 1] and (number[i] == 9 or number[n - i - 1] == 9):
+            k -= 1
+            if k < 0:
+                k += 1
+            else:
+                number[i] = 9
+                number[n - i - 1] = 9
+        else:
+            k -= 2
+            if k < 0:
+                k += 2
+                k -= 1
+                if k < 0:
+                    print -1
+                    flag = 1
+                else:
+                    if number[i] > number[n - i - 1]:
+                        number[n - i - 1] = number[i]
+                    else:
+                        number[i] = number[n - i - 1]
+            else:
+                number[i] = 9
+                number[n - i - 1] = 9
+
+
+if n % 2 == 0:
+    pass
+else:
+    if k > 0 and number[n / 2] != 9:
+        number[n / 2] = 9
+
+if flag == 0:
+    result = [str(i) for i in number]
+    print ''.join(result)
+
+"""
 number = map(lambda x: int(x), list(number))
 digit_not_equal = 0
 digit_is_nine = 0
@@ -33,3 +87,4 @@ else:
 
     result = [str(i) for i in number]
     print ''.join(result)
+"""
